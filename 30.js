a2edca5a7ddc38cd9e34787b01a21d0121fceb36
Sang2522 <96256292+Sang2522@users.noreply.github.com>
2023-07-28 21:37:34 +0700

    Delete 30.js

--- 30.js
+++
@@ -1,9 +0,0 @@
-  let counts = setInterval(updated);
-  let upto = 1000;
-  function updated() {
-      let count = document.getElementById("counter");
-      count.innerHTML = --upto;
-      if (upto === 30) {
-          clearInterval(counts);
-      }
-  }
